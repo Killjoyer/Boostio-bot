d8361b11bd67f1e02800a1fc114454a342a1d6c8
Pam <pamhome21@gmail.com>
2020-10-09 02:56:24 +0500

    fix: style: wrong case in .java files

---
+++ src/main/java/org/tbplusc/app/util/HttpGetter.java
@@ -0,0 +1,26 @@
+package org.tbplusc.app.util;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+
+public class HttpGetter {
+
+    private HttpGetter() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * Returns <code>org.jsoup.nodes.Document</code> from the given URL string. #TODO
+     * 
+     * @param url The URL address with the desired document.
+     * @return URL when the operation is successful; null otherwise.
+     */
+    public static org.jsoup.nodes.Document getDocumentFromUrl(String url) throws IOException {
+        return Jsoup.connect(url).ignoreContentType(true).get();
+    }
+
+    public static String getBodyFromUrl(String url) throws IOException {
+        return getDocumentFromUrl(url).body().text();
+    }
+}
