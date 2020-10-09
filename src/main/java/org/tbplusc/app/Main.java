72d88c89533b1af907740b9efc52694c65fa778b
ninele7 <oleg.belohohlov01@gmail.com>
2020-10-09 15:49:38 +0500

    fix: removed double files; refactor: removed useless code

--- src/main/java/org/tbplusc/app/Main.java
+++ src/main/java/org/tbplusc/app/Main.java
@@ -6,19 +6,7 @@ import org.tbplusc.app.discordinteraction.DefaultChatState;
 import org.tbplusc.app.discordinteraction.MessageHandler;
 import reactor.core.publisher.Mono;
 
-import java.util.logging.Logger;
-
 public class Main {
-    public String mergeStrings(String[] args) {
-        var output = new StringBuilder();
-        for (int i = 0; i < args.length; i++) {
-            output.append(args[i]);
-        }
-        return output.toString();
-    }
-
-    private static final Logger logger = Logger.getLogger("Main");
-
     public static void main(String[] args) {
         final var token = System.getenv("DISCORD_TOKEN");
         final var client = DiscordClient.create(token);
--- src/test/java/org/tbplusc/app/MainTest.java
+++ src/test/java/org/tbplusc/app/MainTest.java
@@ -1,12 +1,5 @@
 package org.tbplusc.app;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class MainTest {
-    @Test
-    public void testMergeStringsCorrectly() {
-        Main classUnderTest = new Main();
-        assertEquals("MergedStrings", classUnderTest.mergeStrings(new String[] { "Merged", "Strings" }));
-    }
+
 }
