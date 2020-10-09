2ed584096d29e7d0e7930507a9da08b281caff79
ninele7 <oleg.belohohlov01@gmail.com>
2020-10-09 17:07:21 +0500

    feat: added slf4j with "simple" backend (example of usage in main)

--- src/main/java/org/tbplusc/app/Main.java
+++ src/main/java/org/tbplusc/app/Main.java
@@ -2,12 +2,17 @@ package org.tbplusc.app;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tbplusc.app.discordinteraction.DefaultChatState;
 import org.tbplusc.app.discordinteraction.MessageHandler;
 import reactor.core.publisher.Mono;
 
 public class Main {
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Application started");
         final var token = System.getenv("DISCORD_TOKEN");
         final var client = DiscordClient.create(token);
         DefaultChatState.registerDefaultCommands();
